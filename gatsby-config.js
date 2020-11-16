c9311f35e26564fa49c836f50c6dfce3771d5de6
xMartin <xmartin@siarp.de>
2020-11-16 01:06:49 +0100

    Update site title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Tailwind`,
-    description: `Gatsby starter styled with Tailwind`,
-    author: `@taylorbryant`,
+    title: `Flat Volleyball`,
+    description: `Enjoy the fun of Beach Volleyball without a net and on any surface`,
+    author: `Martin Stadler`,
   },
   plugins: [
     `gatsby-plugin-eslint`,
