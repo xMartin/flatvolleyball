5eca60b6772fc827d4f9aa75e2356615280986e5
xMartin <xmartin@siarp.de>
2021-01-04 02:00:53 +0100

    Disable web manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,18 +12,18 @@ module.exports = {
   plugins: [
     `gatsby-plugin-eslint`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-tailwind`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: fullConfig.theme.colors.white,
-        theme_color: fullConfig.theme.colors.teal["400"],
-        display: `minimal-ui`,
-        icon: `src/images/logo.svg`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-tailwind`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: fullConfig.theme.colors.white,
+    //     theme_color: fullConfig.theme.colors.teal["400"],
+    //     display: `minimal-ui`,
+    //     icon: `src/images/logo.svg`,
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-postcss`,
       options: {
